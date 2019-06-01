466556f185a2d548e27c1914075b710da9ad2dc3
developer-kikikaikai <kikikaikaikai@gmail.com>
2019-06-02 01:06:43 +0900

    Add HTTP status in all endpoints

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"github.com/savaki/swag"
 	"github.com/savaki/swag/endpoint"
 	"github.com/savaki/swag/swagger"
+	"log"
 )
 
 func handle(c *gin.Context) {
 	io.WriteString(c.Writer, "Insert your code here")
+	log.Println("handle")
 }
 
 type HTTPHeaderField struct {
@@ -21,6 +23,7 @@ type HTTPHeaderField struct {
 
 type HTTPResponse struct {
 	Header	[] HTTPHeaderField `json:header,omitempty`
+	Status int `json:status`
 	Body string `json:body`
 }
 
@@ -31,9 +34,17 @@ type Endpoint struct {
 }
 
 func main() {
+	/*add endpoint /endpints to regist some request */
 	rest_post := endpoint.New("post", "/endpoints", "Add a new endpoint.",
 		endpoint.Handler(handle),
 		endpoint.Description("Add a new endpoint which you want to use. You can add response format"),
+		endpoint.Body(Endpoint{}, "Endpoint object. You can set endpoint, method and response format.", true),
+		endpoint.Response(http.StatusOK, "Success.", "Successfully added endpoint"),
+	)
+	rest_put := endpoint.New("put", "/endpoints/{endpoint}", "Update the endpoint.",
+		endpoint.Handler(handle),
+		endpoint.Description("Update the endpoint which you want to use."),
+		endpoint.Path("endpoint", "string", "endpoint.", true),
 		endpoint.Body(Endpoint{}, "Endpoint object. You can set endpoint, method and response format.", true),
 		endpoint.Response(http.StatusOK, "Success.", "Successfully added endpoint"),
 	)
@@ -49,7 +60,7 @@ func main() {
 	)
 
 	api := swag.New(
-		swag.Endpoints(rest_post, rest_get, rest_delete),
+		swag.Endpoints(rest_post, rest_put, rest_get, rest_delete),
 		swag.Title("API server simulator"),
 		swag.Version("0.0.1"),
 		swag.Description("This is the simulator of API server. You can regist REST API endpoint with response."),
@@ -61,12 +72,25 @@ func main() {
 	api.Walk(func(path string, endpoint *swagger.Endpoint) {
 		h := endpoint.Handler.(func(c *gin.Context))
 		path = swag.ColonPath(path)
-
 		router.Handle(endpoint.Method, path, h)
 	})
 
-	enableCors := true
+	/*any request handling*/
+	router.Use(sampleMiddleware())
+
+	/*Add case to disable swagger endpoint*/
+	enableCors := false
 	router.GET("/swagger", gin.WrapH(api.Handler(enableCors)))
 
 	http.ListenAndServe(":8080", router)
 }
+
+	func sampleMiddleware() gin.HandlerFunc {
+		return func(c *gin.Context) {
+
+			log.Println("before logic")
+			//c.String(http.StatusBadRequest, "Bad request")
+			c.Next()
+			log.Println("after logic")
+		}
+	}
