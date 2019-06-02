2c5c7084d85bc4f764ef8405399459ce22375a06
developer-kikikaikai <kikikaikaikai@gmail.com>
2019-06-02 09:33:17 +0900

    Implement entity logic

--- entities/defined_endpoints.go
+++ entities/defined_endpoints.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/developer-kikikaikai/api_server_simulator/datatypes"
+	"github.com/developer-kikikaikai/api_server_simulator/factory"
 )
 
 type DefinedEndpoints struct {
@@ -12,21 +13,34 @@ type DefinedEndpoints struct {
 var definedEndpoints = DefinedEndpoints{}
 
 func (this *DefinedEndpoints) GetAll() []datatypes.Endpoint {
-	return nil
+	return factory.GetDataStorageAccesser().GetAll()
 }
 
 func (this *DefinedEndpoints) Get(uri string, method string) (datatypes.Endpoint, error) {
-	return nil, errors.New("Not implement yet")
+	if factory.GetDefinedEndpointController().IsHandled(uri, method) {
+		return factory.GetDataStorageAccesser().Get(uri, method), nil
+	} else {
+		return _, errors.New("No data related to uri and method")
+	}
 }
+
 func (this *DefinedEndpoints) Add(endpoint Endpoint) error {
-	return errors.New("Not implement yet")
+	return factory.GetDataStorageAccesser().Add(endpoint)
 }
+
 func (this *DefinedEndpoints) Update(endpoint Endpoint) error {
-	return errors.New("Not implement yet")
+	return factory.GetDataStorageAccesser().Update(endpoint)
 }
+
 func (this *DefinedEndpoints) Delete(endpoint Endpoint) error {
-	return errors.New("Not implement yet")
+	return factory.GetDataStorageAccesser().Delete(endpoint)
 }
-func (this *DefinedEndpoints) GetResponse(uri string, method string) (datatypes.HTTPResponse, error) {
-	return nil, errors.New("Not implement yet")
+
+func (this *DefinedEndpoints) GetResponse(uri string, method string) (*datatypes.HTTPResponse, error) {
+	api := factory.GetDefinedEndpointController()
+	if api.IsHandled(uri, method) {
+		return api.GetResponse(uri, method)
+	} else {
+		return errors.New("No data related to uri and method")
+	}
 }
