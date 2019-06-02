32a59bf5a26afd885542d150dd6f95bbf9dd407f
developer-kikikaikai <kikikaikaikai@gmail.com>
2019-06-02 10:58:11 +0900

    Remove get one endpoint method, because there is no case to use it now.

--- repository/data_storage_accesser.go
+++ repository/data_storage_accesser.go
@@ -6,7 +6,6 @@ import (
 
 type DataStorageAccesser interface {
 	GetAll() []datatypes.Endpoint
-	Get(uri string, method string) datatypes.Endpoint
 	Add(endpoint *datatypes.Endpoint) error
 	Update(endpoint *datatypes.Endpoint) error
 	Delete(uri string, method string) error
